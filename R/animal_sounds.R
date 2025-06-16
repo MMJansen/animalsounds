f2acf44f0a364fc53da605160e13f400559aa996
MMJansen <MMJansen@users.noreply.github.com>
2025-06-16 11:36:39 +0200

    update function documentation

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,3 +1,12 @@
+#' Animal Sounds Is Just Practising Package Building
+#'
+#' @param animal  type of animal
+#' @param sound he soudn it makes, preferably the matching sound ;)
+#'
+#' @returns A sentence with the animal and the osund pasted together
+#' @export
+#'
+#' @examples animal_sounds("cat", "miauw")
 animal_sounds <- function(animal, sound) {
   stopifnot(is.character(animal) & length(animal) == 1)
   stopifnot(is.character(sound) & length(sound) == 1)
